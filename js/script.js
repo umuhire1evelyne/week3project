48f5347e1ba363f452c397aa8413b87f50373230
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-07-28 16:31:53 +0200

    i have added also the script

--- js/script.js
+++ js/script.js
@@ -26,6 +26,42 @@ $(document).ready(function(){
     });
     });
 $(document).ready(function(){
+    $("#work3").hover(function(){
+            $(this).stop().css({opacity:.3},30);
+            $(this).fadein();
+        }, function(){
+            $(this).stop().css({opacity:1},500);
+            $(this).fadeout();
+        });
+        });
+$(document).ready(function(){
+    $("#work4").hover(function(){
+            $(this).stop().css({opacity:.3},30);
+            $(this).fadein();
+        }, function(){
+            $(this).stop().css({opacity:1},500);
+            $(this).fadeout();
+        });
+        });
+$(document).ready(function(){
+    $("#work5").hover(function(){
+            $(this).stop().css({opacity:.3},30);
+            $(this).fadein();
+        }, function(){
+            $(this).stop().css({opacity:1},500);
+            $(this).fadeout();
+        });
+        });
+$(document).ready(function(){
+    $("#work6").hover(function(){
+            $(this).stop().css({opacity:.3},30);
+            $(this).fadein();
+        }, function(){
+            $(this).stop().css({opacity:1},500);
+            $(this).fadeout();
+        });
+        });
+$(document).ready(function(){
     $("#work1").hover(function(){
             $(this).stop().css({opacity:.3},30);
             $(this).fadein();
