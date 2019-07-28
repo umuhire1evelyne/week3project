8cc5a8f693c53467984703768d2b5caf7f0ccdbc
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-07-28 16:28:14 +0200

    i have added also the script

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("work").hover(function(){
+        $(this).stop().css({opacity:.3},30);
+        $(this).fadein();
+    }, function(){
+        $(this).stop().css({opacity:1},500)
+        $(this).fadeout();
+    });
+});
+$(document).ready(function(){
+    $("#work1").hover(function(){
+        $(this).stop().css({opacity:.3},30);
+        $(this).fadein();
+    }, function(){
+        $(this).stop().css({opacity:1},500);
+        $(this).fadeout();
+    });
+    });
+$(document).ready(function(){
+    $("#work2").hover(function(){
+        $(this).stop().css({opacity:.3},30);
+        $(this).fadein();
+    }, function(){
+        $(this).stop().css({opacity:1},500);
+        $(this).fadeout();
+    });
+    });
+$(document).ready(function(){
+    $("#work1").hover(function(){
+            $(this).stop().css({opacity:.3},30);
+            $(this).fadein();
+        }, function(){
+            $(this).stop().css({opacity:1},500);
+            $(this).fadeout();
+        });
+        });
