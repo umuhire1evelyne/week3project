c3bc9c0c71e09bca9c98d22c0fc5fd91c8ef5403
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-07-30 09:46:33 +0200

    i have added some codes in js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,115 @@
 $(document).ready(function() {
     $(".our").click(function(){
-        $(".par1").toggle();
-            $("#1").toggle();    
+        $("#one").toggle();
+            $(".par").toggle();    
     });
+
     $(".all").click(function(){
-        $(".par2").toggle();
-            $("#2").toggle();
+        $("#two").toggle();
+            $(".para").toggle();    
     });
     $(".planning").click(function(){
-        $(".par3").toggle();
-            $("#3").toggle();
+        $("#three").toggle();
+            $(".parag").toggle();    
     });
-    $(".hover").hide();
-    $(".work").animate({
-        opacity:1
+
+    $(".hover1").hide();
+    $(".work1").animate({
+        opacity:0.7
     });
-    $(".work").hover(function(){
-        $(this).stop().animate({opacity:.4}, 200);
-        $(".hover").fadein();
+    $(".work1").hover(function(){
+        $(this).stop()
+        $(".hover1").fadeIn();
     }, function(){
-        $(this).stop().animate({opacity:2},500);
-        $(".hover").fadeout();
+        $(this).stop()
+        $(".hover1").fadeOut();
 });
+
+$(".hover2").hide();
+    $(".work2").animate({
+        opacity:0.7
+    });
+    $(".work2").hover(function(){
+        $(this).stop()
+        $(".hover2").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover2").fadeOut();
+});
+
+$(".hover3").hide();
+    $(".work3").animate({
+        opacity:0.7
+    });
+    $(".work3").hover(function(){
+        $(this).stop()
+        $(".hover3").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover3").fadeOut();
+});
+
+$(".hover4").hide();
+    $(".work4").animate({
+        opacity:0.7
+    });
+    $(".work4").hover(function(){
+        $(this).stop()
+        $(".hover4").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover4").fadeOut();
+});
+
+$(".hover5").hide();
+    $(".work5").animate({
+        opacity:0.7
+    });
+    $(".work5").hover(function(){
+        $(this).stop()
+        $(".hover5").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover5").fadeOut();
+});
+
+$(".hover6").hide();
+    $(".work6").animate({
+        opacity:0.7
+    });
+    $(".work6").hover(function(){
+        $(this).stop()
+        $(".hover6").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover6").fadeOut();
+});
+
+$(".hover7").hide();
+    $(".work7").animate({
+        opacity:0.7
+    });
+    $(".work7").hover(function(){
+        $(this).stop()
+        $(".hover7").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover7").fadeOut();
+});
+
+$(".hover8").hide();
+    $(".work8").animate({
+        opacity:0.7
+    });
+    $(".work8").hover(function(){
+        $(this).stop()
+        $(".hover8").fadeIn();
+    }, function(){
+        $(this).stop()
+        $(".hover8").fadeOut();
+});
+});
+
 // $(document).ready(function() {
 //     $("#wo").hide();
 //     $("#work").animate({
